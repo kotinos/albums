0d823446d9cc18ea1d54ef975b96b3793d8fea15
kotinos <alin33@buffalo.edu>
2018-04-22 03:15:38 -0400

    Return all titles from api

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,14 +3,23 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = { albums: [] };
+
   componentWillMount() {
-    axios.get('https://rallycoding.herokuapp.com/api/music_albums');
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
   }
 
   render() {
+    console.log(this.state);
+
   return (
       <View>
-        <Text>Album List!!!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
