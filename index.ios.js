1e8053be7ce27dedc98fb18bb696ad2b6737e33b
kotinos <alin33@buffalo.edu>
2018-04-22 02:19:37 -0400

    Axios Get Request
    Added json get request

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
-import AlbumList from './src/components/AlbumList'
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
   <View>
-  <Header headerText={'Albums'} />
-  <AlbumList />
+    <Header headerText={'Albums'} />
+    <AlbumList />
   </View>
 );
 
--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums');
+  }
+
   render() {
   return (
       <View>
