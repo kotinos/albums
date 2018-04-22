86659088ee1a54dd96779e2d9ecd12cd5da64263
kotinos <alin33@buffalo.edu>
2018-04-22 19:20:02 -0400

    Added Image Functionality
    Created Image Sections into cards

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,62 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+//POSITIONING
+//image | (album title over album artist)
+
+const AlbumDetail = ({ album }) => {
+
+  //destructure from album
+const { title, artist, thumbnail_image, image } = album;
+
+// another destructure
+const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle, headerTextStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+      <View style={thumbnailContainerStyle}>
+        <Image
+          style={thumbnailStyle}
+          source={{ uri: thumbnail_image }}
+        />
+      </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      <CardSection>
+        <Image source={{ uri: image }} />
       </CardSection>
     </Card>
   );
 };
+
+//must specify style for image to appear
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18,
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  }
+};
+
+//image tag
+//CardSection->image->text->text<-CardSection
 
 export default AlbumDetail;
